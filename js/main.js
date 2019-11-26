0d31f2cacbe7cca4638fe3b819a61e463dc3a68a
Larry Cohen <cohlar@gmail.com>
2019-11-26 17:51:20 +0200

    close button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // GEEK
-// add game reset function
-// add header with logo and button to reset
-// win modal with button for new game
+// Add the ability to change the game theme (both images and card pattern).
+// Make the basic layout the “easy” level, add levels medium and hard (with more 18 and 24 cards).
+// NINJA
+// Add flipping animation effect for the card. (and for the modal...)
+// Add a high score functionality, that will save the name of the person with the least amounts of wrong guesses.
 
 "use strict";
 
@@ -18,6 +20,7 @@
         session: {
             numImages: 6,               // beginner mode
             hideImageLagTime: 2000,     // in ms
+            displayModalLagTime: 1000,  // in ms
             selectedImages: undefined,
             imageRevealed: undefined,
             startScore: 50,
@@ -28,16 +31,11 @@
         },
     };
 
-    // MemoryGame.setNewGameButton = function () {
-    //     $(".new-game-btn").on("click", this.start);
-    // };
-
     MemoryGame.start = function () {
         MemoryGame.removeModal();
         MemoryGame.session.setToDefault();
         MemoryGame.generateHTML();
         $(".game-img").on("click", MemoryGame.revealImage);
-        // $(window).on("resize", MemoryGame.sizeImages);
     };
 
     MemoryGame.session.setToDefault = function () {
@@ -58,7 +56,6 @@
         this.session.selectedImages.forEach(function (imageName, index) {
             $("#game").append($(`<img src="${MemoryGame.images.path + MemoryGame.images.unknownImageName}" id="${index}" class="game-img" >`));
         });
-        // this.sizeImages();
     };
 
     MemoryGame.revealImage = function () {
@@ -68,8 +65,6 @@
                 MemoryGame.session.imageRevealed = this.id;
             } else if (this.id !== MemoryGame.session.imageRevealed) {
                 if (MemoryGame.session.selectedImages[MemoryGame.session.imageRevealed] === MemoryGame.session.selectedImages[this.id]) {
-                    // $(".game-img").off("click", MemoryGame.revealImage);
-                    // setTimeout((() => $(".game-img").on("click", MemoryGame.revealImage)), MemoryGame.session.hideImageLagTime);
                     MemoryGame.session.rightAnswerCount++;
                     MemoryGame.session.imageRevealed = null;
                 } else {
@@ -81,26 +76,11 @@
             }
         }
         if (MemoryGame.session.rightAnswerCount === MemoryGame.session.numImages) {
-            MemoryGame.displayWinModal();
+            setTimeout(MemoryGame.displayWinModal, MemoryGame.session.displayModalLagTime);
             MemoryGame.session.isCompleted = true;
-            // $(".game-img").off("click", MemoryGame.revealImage);
+            $(".game-img").off("click", MemoryGame.revealImage);
         }
     };
-
-    // MemoryGame.sizeImages = function () {
-    //     const gameWidth = window.innerWidth;
-    //     const gameHeight = window.innerHeight - parseInt($("header").css("height")) - parseInt($("#score-container").css("height"));
-
-    //     console.log(gameWidth, gameHeight);
-        
-    //     if (gameHeight < gameWidth) {
-    //         $(".game-img").css({
-    //             "height": gameHeight/4 + "px",
-    //             "width": gameHeight/4 + "px",
-                
-    //         });
-    //     }
-    // };
 
     MemoryGame.hideImage = function () {
         this.src = MemoryGame.images.path + MemoryGame.images.unknownImageName;
@@ -133,30 +113,18 @@
     };
 
     MemoryGame.displayWinModal = function () {
-        this.createModal();
+        MemoryGame.createModal();
         $("#modal").append(`
             <h1>Congratulations, you won!</h1>
             <p>
-                Your final score is <span id="final-score">100</span>
+                Your final score is <span id="final-score"></span>
             </p>
             <button class="new-game-btn">Start a New Game</button>
             <img id="win-gif">
         `);
-        $(".new-game-btn").on("click", this.start);
-        this.displayRandomGif();
-    };
-    
-    MemoryGame.createModal = function () {
-        $("body").append(`
-            <div class="modal-background">
-                <div id="modal">
-                </div>
-            </div>
-        `);
-    };
-
-    MemoryGame.removeModal = function () {
-        $(".modal-background").remove();
+        $("#final-score").html(MemoryGame.session.score);
+        $(".new-game-btn").on("click", MemoryGame.start);
+        MemoryGame.displayRandomGif();
     };
 
     MemoryGame.displayRandomGif = async function () {
@@ -176,8 +144,21 @@
             $("#win-gif").attr("src", "https://media3.giphy.com/media/l3q2Z6S6n38zjPswo/200_d.gif");
         }
     }
+    
+    MemoryGame.createModal = function () {
+        $("body").append(`
+            <div class="modal-background">
+                <div id="modal">
+                    <div id="close-modal">X</div>
+                </div>
+            </div>
+        `);
+        $("#close-modal").on("click", this.removeModal);
+    };
 
-    // MemoryGame.displayWinModal();
+    MemoryGame.removeModal = function () {
+        $(".modal-background").remove();
+    };
 
     $(".new-game-btn").on("click", MemoryGame.start);
     MemoryGame.start();
